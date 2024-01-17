bc0e9608285e93d0417dc674dfe9a86103c332cf
vyktron <vyktron@proton.me>
2024-01-17 09:03:51 +0100

    compose ok

--- master/consumer/consumer.py
+++ master/consumer/consumer.py
@@ -33,16 +33,40 @@ if __name__ == "__main__":
     # Replace 'your_topic_name' with the desired topic name
     topic_name = 'coordinates'
 
-    print("Creating Kafka topic...")
-    
-    # Wait for Kafka to be up and running
-    time.sleep(30)
+
+    print("Connecting to Kafka...", flush=True)
 
     # Get the bootstrap servers from the environment variable
     bootstrap_servers = os.environ.get('KAFKA_REST_BOOTSTRAP_SERVERS')
-
+    attempts = 1
     # Create a Kafka consumer
-    consumer = KafkaConsumer(bootstrap_servers=bootstrap_servers)
+    while attempts < 4:
+        try:
+            consumer = KafkaConsumer(bootstrap_servers=bootstrap_servers)
+            break
+        except:
+            print(f"Attempt {attempts}/3: Kafka not ready, new attempt in 5s...", flush=True)
+            time.sleep(5) ; attempts += 1
+    
+    if attempts == 4:
+        print("Kafka not ready, exiting...", flush=True)
+        exit(1)
+    
+    print("Connected to Kafka.", flush=True)
 
     # Call the function to create the Kafka topic
     create_kafka_topic(bootstrap_servers, topic_name)
+
+    print("Ready to consume messages.", flush=True)
+
+    # Subscribe to the topic
+    consumer.subscribe([topic_name])
+
+    # Poll for messages
+    while True:
+        message = consumer.poll(timeout_ms=1000)
+        if message:
+            print(message, flush=True)
+        time.sleep(1)
+
+    
